db8da94615bceda24edbc4e57a0d4723694a46c6
aeromouse007 <aeromouse007@gmail.com>
2022-10-06 22:19:19 +0800

    final age 3

--- age.py
+++ age.py
@@ -1,17 +1,16 @@
 driving = input('請問你有沒有開過車?')
-if driving != '有' and driving != '沒有':
-    print('只能回答有/沒有')
+if driving != 'yes' and driving != 'no':
+    print('工三小')
     raise SystemExit
-
 age = input('請問你的年齡?')
 age = int(age)
-if driving == '有':
+if driving == 'yes':
     if age >= 18:
         print('你通過測驗')
     else:
         print('你怎麼會開過車?')	
-elif driving == '沒有':
+elif driving == 'no':
     if age >= 18:
-        print('快去考駕照')
+        print('去考')
     else:
-        print('乖孩子')   
+        print('以後再去考') 
